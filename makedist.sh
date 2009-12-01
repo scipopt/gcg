2b8f309b58b6fb349b555949a8c291388c1d6a13
Gerald Gamrath <gamrath@zib.de>
2009-12-01 10:27:00 +0000

    added README AND COPYING files, makedist for creating an archive of the project and updated the CAHNGELOG file version will be set to v040 in the following

---
+++ makedist.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# For release versions, only use VERSION="x.x.x".
+# For development versions, use VERSION="x.x.x.x" with subversion number.
+VERSION="1.2.0.3"
+NAME="gcg-$VERSION"
+rm -f $NAME
+ln -s . $NAME
+if test ! -e release
+then
+    mkdir release
+fi
+rm -f release/$NAME.tgz
+tar --no-recursion --ignore-failed-read -cvzhf release/$NAME.tgz \
+--exclude="*CVS*" \
+--exclude="*cvs*" \
+--exclude="*~" \
+--exclude=".*" \
+$NAME/COPYING $NAME/INSTALL $NAME/CHANGELOG $NAME/Makefile $NAME/doc/* \
+$NAME/check/check.sh $NAME/check/evalcheck.sh $NAME/check/check.awk \
+$NAME/check/shortmiplib.test $NAME/check/shortmiplib.solu $NAME/check/cmpres.awk \
+$NAME/check/cpmp100.test $NAME/check/cpmp100.solu \ 
+$NAME/settings/*.set \
+$NAME/src/depend.* \
+$NAME/src/*.c $NAME/src/*.h \
+$NAME/check/instances/cpmp/*100* \
+rm -f $NAME
+echo ""
+echo "finished"
