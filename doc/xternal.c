19d17a386a9d5cd8ffa70404c585ed3a28d76c46
Martin Bergner <bergner@or.rwth-aachen.de>
2012-07-26 21:54:06 +0200

    beautify structure saving information

--- doc/xternal.c
+++ doc/xternal.c
@@ -436,6 +436,13 @@ GCG> q
  * \htmlinclude authors.inc
  */
 
+
+/**@page PARAMS GCG default parameter settings
+ *
+ * \verbinclude inc/parameters.set
+ */
+
+
 /*--+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
 /**@page FILEFORMATS Input file formats supported by GCG.
  *
@@ -925,11 +932,6 @@ GCG> q
  *
  */
 
-/**@page PARAMS GCG default parameter settings
- *
- * \verbinclude inc/parameters.set
- */
-
  /*--+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
 /**@page BRANCH How to add branching rules to GCG
  *
@@ -1037,23 +1039,45 @@ GCG> q
  */
 
  /*--+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
-/**@page DEC_DECOMP Storing structure information
+/**@page DEC_DECOMP How to store the structure information
  *
  * struct_decomp.h is responsible for storing structure information. The memory has to be allocated by caller and is freed
- * later. You should use getter and setter functions ins pub_decomp.h to fill this structure.
+ * later. You have to use getter and setter functions ins pub_decomp.h to fill this structure.
  *
- * Very quick until more elaborate, these are the relevant fields of the structure:
- *  - subscipconss - an array of array of constraints in each block - array[blocknr][constraintid]
- *  - nsubscipconss - an array of the number of constraints in each block
- *  - subscipvars - an array of arrays of variables in each block - array[blocknr][varid]
- *  - nsubscipvars - an array of the number of constraints in each blocks
- *  - nblocks - number of blocks
- *  - type - Type of the decomposition (DEC_DECTYPE_ARROWHEAD is the most general)
- *  - constoblock - SCIP_HASHMAP linking constraints to blocks
- *  - vartoblock - SCIP_HASHMAP linking variables to blocks
- *  - linkingvars - array of linking variables (to be in the master)
- *  - nlinkingvars - number of linking variables
- *  - linkingconss - array of linking constraints (to be in the master)
- *  - nlinkingconss - number of linking constraints
+ * Very quick until more elaborate, these are the relevant fields of the structure and its basic usage:
+ *  - <code>subscipcons   </code>
+ *   - an array of array of constraints in each block
+ *   - Usage: <code>subscipcons[b][c]</code> is constraint <em>c</em> in block <em>b</em>
+ *  - <code>nsubscipconss </code>
+ *   - an array of the number of constraints in each block
+ *   - Usage: <code>nsubscipcons[b]</code> gives you the number of constraints of block <em>b</em>
+ *  - <code>subscipvars   </code>
+ *   - an array of arrays of variables in each block
+ *   - Usage: <code>subscipvars[b][v]</code> is variable <em>v</em> in block <em>b</em>
+ *  - <code>nsubscipvars  </code>
+ *   - an array of the number of variables in each block
+ *   - Usage: <code>nsubscipvars[b]</code> gives you the number of variables of block <em>b</em>
+ *  - <code>nblocks       </code>
+ *   - number of blocks/pricing problems in the matrix resp. reformulation
+ *  - <code>type          </code>
+ *   - Type of the decomposition (DEC_DECTYPE_ARROWHEAD is the most general)
+ *   - Current supported types: DEC_DECTYPE_DIAGONAL, DEC_DECTYPE_BORDERED, DEC_DECTYPE_ARROWHEAD, DEC_DECTYPE_UNKNOWN
+ *  - <code>constoblock   </code>
+ *   - SCIP_HASHMAP linking constraints to blocks
+ *   - Usage: <code>SCIPhashmapGetImage(constoblock, cons)</code> returns <em>b+1</em>, where <em>b</em> is the block of
+ *     constraint cons. This map is somehow inverse of the subscipconss array.
+ *  - <code>vartoblock    </code>
+ *   - SCIP_HASHMAP linking variables to blocks
+ *   - Usage: <code>SCIPhashmapGetImage(vartoblock, var)</code> returns <em>b+1</em>, where <em>b</em> is the block of
+ *     variable var. This map is somehow inverse of the subscipcvars array.
+ *  - <code>linkingvars   </code>
+ *   - array of linking variables (to be in the master)
+ *   - Usage: <code>linkingvars[v]</code> is linking variable number <em>v</em>
+ *  - <code>nlinkingvars  </code>
+ *   - number of linking variables
+ *  - <code>linkingconss  </code>
+ *   - array of linking constraints (to be in the master)
+ *   - Usage: <code>linkingcons[c]</code> is linking constraint number <em>c</em>
+ *  - <code>nlinkingconss </code>
+ *   - number of linking constraints
  */
-
