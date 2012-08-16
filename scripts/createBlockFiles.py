996db50311083e3a72c789521a0246cdb858ae7e
Gerald Gamrath <bzfgamra@zib.de>
2012-08-16 17:12:59 +0200

    added scripts to create block (and mps) files for a testset

---
+++ scripts/createBlockFiles.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import commands
+
+
+if len(sys.argv) == 2:
+    infile = open(sys.argv[1], "r")
+    instances = infile.readlines()
+
+    for instance in instances:
+        instance = instance.strip()
+        print instance
+        if instance.replace(".gz", "").endswith(".lp"):
+            mpsinstance = instance.replace(".gz", "").replace(".lp",".mps")
+            os.system('bin/gcg -c "read check/%s write problem check/%s quit"' %(instance, mpsinstance))
+        else:
+            mpsinstance = instance
+        os.system('bin/gcg -c "read check/%s detect write problem check/%s.ref quit"' %(mpsinstance, mpsinstance))
+        os.system('scripts/ref2block.sh check/%s.ref > check/%s.block' %(mpsinstance, mpsinstance))
+else:
+    print "usage: createBlockFiles.py <testset>"
