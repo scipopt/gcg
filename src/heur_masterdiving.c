2c4cdf449880b5c83f27fb4640d6b1125441a783
Christian Puchert <puchert@or.rwth-aachen.de>
2012-10-18 17:06:27 +0200

    no debug messages in heur_masterdiving.

--- src/heur_masterdiving.c
+++ src/heur_masterdiving.c
@@ -30,7 +30,7 @@
  * @author Tobias Achterberg
  * @author Christian Puchert
  */
-#define SCIP_DEBUG
+
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
 
 #include <assert.h>
