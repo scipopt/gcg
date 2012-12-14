d0c4cb2d15f65f9ebf58c9cfd96db1b6dc62c193
Martin Bergner <bergner@or.rwth-aachen.de>
2012-12-14 09:58:48 +0100

    add further tests to test framework and call certain tests with assertion
    * do not use SCIP_CALL_ABORT, but rather expect SCIP_OKAY as return code

--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -4,6 +4,10 @@
 #include "relax_gcg.h"
 #include "pricer_gcg.h"
 #include "cons_decomp.h"
+#include "reader_blk.h"
+#include "reader_dec.h"
+
+#define SCIP_CALL_EXPECT(x) do { EXPECT_EQ(SCIP_OKAY, (x)); } while(FALSE)
 
 class GcgTest : public ::testing::Test {
  protected:
@@ -148,12 +152,12 @@ SCIP* GcgLibTest::scip = NULL;
 TEST_F(GcgLibTest, FreeTransformTest) {
    SCIP_RESULT result;
    ASSERT_NEAR(41.0, SCIPgetSolTransObj(scip, SCIPgetBestSol(scip)), SCIPfeastol(scip));
-   SCIP_CALL_ABORT( SCIPfreeTransform(scip) );
+   SCIP_CALL_EXPECT( SCIPfreeTransform(scip) );
    ASSERT_EQ(SCIP_STAGE_PROBLEM, SCIPgetStage(scip));
    ASSERT_EQ(0, SCIPconshdlrDecompGetNDecdecomps(scip));
-   SCIP_CALL_ABORT( SCIPpresolve(scip) );
-   SCIP_CALL_ABORT( DECdetectStructure(scip, &result) );
-   SCIP_CALL_ABORT( SCIPsolve(scip) );
+   SCIP_CALL_EXPECT( SCIPpresolve(scip) );
+   SCIP_CALL_EXPECT( DECdetectStructure(scip, &result) );
+   SCIP_CALL_EXPECT( SCIPsolve(scip) );
    ASSERT_NEAR(41.0, SCIPgetSolTransObj(scip, SCIPgetBestSol(scip)), SCIPfeastol(scip));
 
    ASSERT_EQ(SCIP_STATUS_OPTIMAL, SCIPgetStatus(scip));
@@ -162,31 +166,78 @@ TEST_F(GcgLibTest, FreeTransformTest) {
 TEST_F(GcgLibTest, FreeProbTest) {
    SCIP_RESULT result;
    ASSERT_NEAR(41.0, SCIPgetSolTransObj(scip, SCIPgetBestSol(scip)), SCIPfeastol(scip));
-   SCIP_CALL_ABORT( SCIPfreeProb(scip) );
+   SCIP_CALL_EXPECT( SCIPfreeProb(scip) );
    ASSERT_EQ(0, SCIPconshdlrDecompGetNDecdecomps(scip));
-   SCIP_CALL_ABORT( SCIPreadProb(scip, "check/instances/bpp/N1C1W4_M.BPP.lp", "lp") );
-   SCIP_CALL_ABORT( SCIPpresolve(scip) );
-   SCIP_CALL_ABORT( DECdetectStructure(scip, &result) );
-   SCIP_CALL_ABORT( SCIPsolve(scip) );
+   SCIP_CALL_EXPECT( SCIPreadProb(scip, "check/instances/bpp/N1C1W4_M.BPP.lp", "lp") );
+   SCIP_CALL_EXPECT( SCIPpresolve(scip) );
+   SCIP_CALL_EXPECT( DECdetectStructure(scip, &result) );
+   SCIP_CALL_EXPECT( SCIPsolve(scip) );
    ASSERT_NEAR(41.0, SCIPgetSolTransObj(scip, SCIPgetBestSol(scip)), SCIPfeastol(scip));
 
    ASSERT_EQ(SCIP_STATUS_OPTIMAL, SCIPgetStatus(scip));
 }
 
 TEST_F(GcgLibTest, FreeSolveTest) {
-   SCIP_RESULT result;
    ASSERT_NEAR(41.0, SCIPgetSolTransObj(scip, SCIPgetBestSol(scip)), SCIPfeastol(scip));
-   SCIP_CALL_ABORT( SCIPfreeSolve(scip, FALSE) );
+   SCIP_CALL_EXPECT( SCIPfreeSolve(scip, FALSE) );
    ASSERT_EQ(SCIP_STAGE_TRANSFORMED, SCIPgetStage(scip));
    ASSERT_LE(1, SCIPconshdlrDecompGetNDecdecomps(scip));
-   SCIP_CALL_ABORT( SCIPpresolve(scip) );
-  // SCIP_CALL_ABORT( DECdetectStructure(scip, &result) );
-   SCIP_CALL_ABORT( SCIPsolve(scip) );
+   SCIP_CALL_EXPECT( SCIPpresolve(scip) );
+  // SCIP_CALL_EXPECT( DECdetectStructure(scip, &result) );
+   SCIP_CALL_EXPECT( SCIPsolve(scip) );
    ASSERT_NEAR(41.0, SCIPgetSolTransObj(scip, SCIPgetBestSol(scip)), SCIPfeastol(scip));
 
    ASSERT_EQ(SCIP_STATUS_OPTIMAL, SCIPgetStatus(scip));
 }
 
+class GcgDecTest : public ::testing::Test {
+ protected:
+  static SCIP *scip;
+
+  static void SetUpTestCase() {
+  }
+
+  static void TearDownTestCase() {
+
+  }
+
+   virtual void SetUp() {
+     SCIP_CALL_ABORT( SCIPcreate(&scip) );
+     SCIP_CALL_ABORT( SCIPincludeGcgPlugins(scip) );
+     SCIP_CALL_ABORT( SCIPcreateProb(scip, "test", NULL, NULL, NULL, NULL,NULL, NULL, NULL) );
+     SCIP_CALL_ABORT( SCIPsetIntParam(scip, "display/verblevel", SCIP_VERBLEVEL_NONE) );
+   }
+
+   virtual void TearDown() {
+     SCIP_CALL_ABORT( SCIPfree(&scip) );
+   }
+};
+
+SCIP* GcgDecTest::scip = NULL;
+
+TEST_F(GcgDecTest, ReadDecTest) {
+   SCIP_RESULT result;
+   SCIP_CALL_EXPECT( SCIPreadProb(scip, "check/instances/miplib/noswot.mps", "mps") );
+   SCIP_CALL_EXPECT( SCIPreadDec(scip, "check/instances/miplib/noswot.dec", &result) );
+   ASSERT_EQ(SCIP_SUCCESS, result);
+   ASSERT_EQ(1, SCIPconshdlrDecompGetNDecdecomps(scip));
+   SCIP_CALL_EXPECT( SCIPsetIntParam(scip, "presolving/maxrounds", 0) );
+   SCIP_CALL_EXPECT( SCIPsolve(scip) );
+   ASSERT_EQ(1, SCIPconshdlrDecompGetNDecdecomps(scip));
+   ASSERT_NEAR(-41.0, SCIPgetSolTransObj(scip, SCIPgetBestSol(scip)), SCIPfeastol(scip));
+}
+
+TEST_F(GcgDecTest, ReadBlkTest) {
+   SCIP_RESULT result;
+   SCIP_CALL_EXPECT( SCIPreadProb(scip, "check/instances/bpp/N1C3W1_A.lp", "lp") );
+   SCIP_CALL_EXPECT( SCIPreadBlk(scip, "check/instances/bpp/N1C3W1_A.blk", &result) );
+   ASSERT_EQ(SCIP_SUCCESS, result);
+   ASSERT_EQ(1, SCIPconshdlrDecompGetNDecdecomps(scip));
+   SCIP_CALL_EXPECT( SCIPsetIntParam(scip, "presolving/maxrounds", 0) );
+   SCIP_CALL_EXPECT( SCIPsolve(scip) );
+   ASSERT_EQ(1, SCIPconshdlrDecompGetNDecdecomps(scip));
+   ASSERT_NEAR(16.0, SCIPgetSolTransObj(scip, SCIPgetBestSol(scip)), SCIPfeastol(scip));
+}
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
