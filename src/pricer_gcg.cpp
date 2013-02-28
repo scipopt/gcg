scipopt/gcg

commit eec682f703ed855b1cb926b251c2078e49784efa

fix bug when pricing problem becomes infeasible

src/pricer_gcg.cpp
@@ -1461,7 +1461,7 @@ SCIP_RETCODE ObjPricerGcg::performPricing(
    SCIP_Bool** solisray;
    int maxsols;
    SCIP_RETCODE retcode;
-
+   SCIP_Bool infeasible;
    assert(pricetype->getType() == GCG_PRICETYPE_FARKAS || result != NULL);
 
    assert(nfoundvars != NULL);
@@ -1474,7 +1474,7 @@ SCIP_RETCODE ObjPricerGcg::performPricing(
    successfulmips = 0;
    retcode = SCIP_OKAY;
    *nfoundvars = 0;
-
+   infeasible = FALSE;
    *bestredcost = 0.0;
    *bestredcostvalid = ( SCIPgetLPSolstat(scip_) == SCIP_LPSOLSTAT_OPTIMAL && optimal ? TRUE : FALSE );
    pricinglowerbound = SCIPinfinity(scip_);
@@ -1521,7 +1521,7 @@ SCIP_RETCODE ObjPricerGcg::performPricing(
       if( pricerdata->pricingprobs[prob] == NULL || retcode != SCIP_OKAY)
          goto done;
 
-      if( abortPricing(pricetype, *nfoundvars, solvedmips, successfulmips, optimal) )
+      if( abortPricing(pricetype, *nfoundvars, solvedmips, successfulmips, optimal) || infeasible )
          goto done;
 
       #pragma omp ordered
@@ -1532,7 +1532,13 @@ SCIP_RETCODE ObjPricerGcg::performPricing(
          retcode = private_retcode;
 
          #pragma omp atomic
-         *nfoundvars += countPricedVariables(prob, sols[prob], nsols[prob], solisray[prob] );
+         infeasible = (SCIPgetStatus(pricerdata->pricingprobs[prob]) == SCIP_STATUS_INFEASIBLE);
+
+         if( !infeasible )
+         {
+            #pragma omp atomic
+            *nfoundvars += countPricedVariables(prob, sols[prob], nsols[prob], solisray[prob] );
+         }
 
          if(nvarsfound < *nfoundvars)
          {
@@ -1575,7 +1581,7 @@ SCIP_RETCODE ObjPricerGcg::performPricing(
    /** @todo solve all pricing problems all k iterations? */
    *nfoundvars = 0;
 
-   for( i = 0; i < pricerdata->npricingprobs; ++i )
+   for( i = 0; i < pricerdata->npricingprobs && !infeasible; ++i )
    {
       int prob;
       prob = pricerdata->permu[i];
@@ -1648,6 +1654,14 @@ SCIP_RETCODE ObjPricerGcg::performPricing(
 
    /* free the pricingproblems if they exist and need to be freed */
    SCIP_CALL( freePricingProblems() );
+
+   if( result != NULL )
+   {
+      if( *nfoundvars > 0 || infeasible )
+         *result = SCIP_SUCCESS;
+      else if( *bestredcostvalid == FALSE )
+         *result = SCIP_DIDNOTRUN;
+   }
 
    return SCIP_OKAY;
 }

src/solver_mip.c
@@ -461,7 +461,7 @@ SCIP_RETCODE solveProblem(
    int                   maxsols,            /**< size of preallocated array */
    int*                  nsols,              /**< pointer to store number of solutions */
    SCIP_Real*            lowerbound,         /**< pointer to store lower bound */
-   SCIP_STATUS*          result              /**< pointer to store pricing problem status */
+   SCIP_STATUS*          status              /**< pointer to store pricing problem status */
    )
 {
    SCIP_SOL** probsols;
@@ -478,11 +478,12 @@ SCIP_RETCODE solveProblem(
    if( retcode != SCIP_OKAY )
    {
       SCIPwarningMessage(pricingprob, "Encountered non recoverable issues solving pricingproblem, ignoring problem\n");
-      *result = SCIP_STATUS_UNKNOWN;
+      *status = SCIP_STATUS_UNKNOWN;
       return SCIP_OKAY;
    }
    SCIPdebugMessage("MIP pricing solver: status = %d\n", SCIPgetStatus(pricingprob));
 
+   *status = SCIPgetStatus(pricingprob);
 
    /* all SCIP statuses handled so far */
    assert(SCIPgetStatus(pricingprob) == SCIP_STATUS_OPTIMAL
@@ -499,6 +500,14 @@ SCIP_RETCODE solveProblem(
    /* the pricing problem was declared to be (infeasible or) unbounded and we should have a primal ray at hand,
     * so copy the primal ray into the solution structure and mark it to be a primal ray
     */
+
+   if( SCIPgetStatus(pricingprob) == SCIP_STATUS_INFEASIBLE )
+   {
+      SCIPdebugMessage("Pricing is infeasible, abort immediately.\n");
+      *status = SCIP_STATUS_INFEASIBLE;
+      return SCIP_OKAY;
+   }
+
    if( problemIsUnbounded(pricingprob) )
    {
       if( !SCIPhasPrimalRay(pricingprob) )
@@ -510,13 +519,13 @@ SCIP_RETCODE solveProblem(
 
       *nsols = 1;
       solisray[0] = TRUE;
-      *result = SCIP_STATUS_UNBOUNDED;
+      *status = SCIP_STATUS_UNBOUNDED;
    }
    /* the solving process was interrupted, so we have no solutions and set the status pointer accordingly */
    else if( problemIsInterrupted(pricingprob) )
    {
       *nsols = 0;
-      *result = SCIPgetStatus(pricingprob);
+      *status = SCIPgetStatus(pricingprob);
    }
    /* the pricing problem has an unbounded solution but finite solution, resolve it and extract a finite solution if necessary */
    else if( problemHasUnboundedSolution(pricingprob) )
@@ -524,7 +533,7 @@ SCIP_RETCODE solveProblem(
       SCIP_SOL* sol = NULL;
       SCIP_Bool isray;
       SCIP_Bool freesol;
-      *result = SCIP_STATUS_UNKNOWN;
+      *status = SCIP_STATUS_UNKNOWN;
       SCIP_CALL( inferFiniteSolution(pricingprob, SCIPgetBestSol(pricingprob), &sol, &isray, &freesol) );
       if( sol != NULL )
       {
@@ -532,7 +541,7 @@ SCIP_RETCODE solveProblem(
          sols[0] = sol;
          solisray[0] = isray ? TRUE: freesol ? 2 : FALSE;
          SCIPdebugMessage("# changing solution\n");
-         *result = SCIP_STATUS_OPTIMAL;
+         *status = SCIP_STATUS_OPTIMAL;
       }
    }
    /* the pricing problem was solved to optimality, copy all solutions found into the solution arrays */
@@ -553,9 +562,9 @@ SCIP_RETCODE solveProblem(
          {
             SCIPwarningMessage(pricingprob, "solution of pricing problem %d not feasible:\n", probnr);
             SCIP_CALL( SCIPcheckSolOrig(pricingprob, probsols[s], &feasible, TRUE, TRUE) );
-            if( *result != SCIP_STATUS_OPTIMAL )
+            if( *status != SCIP_STATUS_OPTIMAL )
             {
-               *result = SCIP_STATUS_UNKNOWN;
+               *status = SCIP_STATUS_UNKNOWN;
             }
          }
 
@@ -576,7 +585,7 @@ SCIP_RETCODE solveProblem(
       if( SCIPgetStatus(pricingprob) == SCIP_STATUS_OPTIMAL )
          *lowerbound = SCIPgetDualbound(pricingprob);
 
-      *result = SCIP_STATUS_OPTIMAL;
+      *status = SCIP_STATUS_OPTIMAL;
       SCIPdebugMessage("pricingproblem found %d sols!\n", *nsols);
    }
 

commit 16c1d7e222f9fc9eb5fff439f46e65bfe5def616

fix additional bugs introduced in eec682f

src/pricer_gcg.cpp
@@ -1462,6 +1462,8 @@ SCIP_RETCODE ObjPricerGcg::performPricing(
    int maxsols;
    SCIP_RETCODE retcode;
    SCIP_Bool infeasible;
+   SCIP_Bool pricinghaserror;
+
    assert(pricetype->getType() == GCG_PRICETYPE_FARKAS || result != NULL);
 
    assert(nfoundvars != NULL);
@@ -1475,6 +1477,7 @@ SCIP_RETCODE ObjPricerGcg::performPricing(
    retcode = SCIP_OKAY;
    *nfoundvars = 0;
    infeasible = FALSE;
+   pricinghaserror = FALSE;
    *bestredcost = 0.0;
    *bestredcostvalid = ( SCIPgetLPSolstat(scip_) == SCIP_LPSOLSTAT_OPTIMAL && optimal ? TRUE : FALSE );
    pricinglowerbound = SCIPinfinity(scip_);
@@ -1507,7 +1510,7 @@ SCIP_RETCODE ObjPricerGcg::performPricing(
       }
    }
 
-   #pragma omp parallel for ordered firstprivate(pricinglowerbound) shared(retcode, optimal, solisray, sols, nsols, maxsols,pricetype,bestredcost,bestredcostvalid,nfoundvars,successfulmips) reduction(+:solvedmips)
+   #pragma omp parallel for ordered firstprivate(pricinglowerbound) shared(retcode, optimal, solisray, sols, nsols, maxsols,pricetype,bestredcost,bestredcostvalid,nfoundvars,successfulmips,infeasible,pricinghaserror) reduction(+:solvedmips)
    for( i = 0; i < pricerdata->npricingprobs; i++ )
    {
       int prob;
@@ -1532,7 +1535,10 @@ SCIP_RETCODE ObjPricerGcg::performPricing(
          retcode = private_retcode;
 
          #pragma omp atomic
-         infeasible = (SCIPgetStatus(pricerdata->pricingprobs[prob]) == SCIP_STATUS_INFEASIBLE);
+         infeasible = infeasible || (SCIPgetStatus(pricerdata->pricingprobs[prob]) == SCIP_STATUS_INFEASIBLE);
+
+         #pragma omp atomic
+         pricinghaserror = pricinghaserror || (status == SCIP_STATUS_UNKNOWN);
 
          if( !infeasible )
          {
@@ -1556,6 +1562,7 @@ SCIP_RETCODE ObjPricerGcg::performPricing(
               SCIPwarningMessage(scip_, "Numerical troubles solving pricing %d (error is %.4g)\n", prob, pricinglowerbound - pricerdata->dualsolconv[prob]);
               *bestredcostvalid = FALSE;
               assert(SCIPisLT(scip_, pricinglowerbound - pricerdata->dualsolconv[prob], 1.0));
+              pricinghaserror = TRUE;
            }
         }
 
@@ -1657,9 +1664,14 @@ SCIP_RETCODE ObjPricerGcg::performPricing(
 
    if( result != NULL )
    {
-      if( *nfoundvars > 0 || infeasible )
+      if( infeasible )
+      {
+         *bestredcost = SCIPinfinity(scip_);
          *result = SCIP_SUCCESS;
-      else if( *bestredcostvalid == FALSE )
+      }
+      else if( *nfoundvars > 0)
+         *result = SCIP_SUCCESS;
+      else if( pricinghaserror )
          *result = SCIP_DIDNOTRUN;
    }
 