db1f880f29a5ee5bfc62d949caa80c5bacad5a6f
Martin Bergner <bergner@or.rwth-aachen.de>
2013-03-08 09:58:19 +0100

    use correct include guards in detectors

--- src/dec_stairheur.h
+++ src/dec_stairheur.h
@@ -34,8 +34,8 @@
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
 
-#ifndef __SCIP_DEC_STAIRHEUR_H__
-#define __SCIP_DEC_STAIRHEUR_H__
+#ifndef GCG_DEC_STAIRHEUR_H__
+#define GCG_DEC_STAIRHEUR_H__
 
 
 #include "scip/scip.h"
