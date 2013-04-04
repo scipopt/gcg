1654d4f7411a95a5c953740a33274bffa2e12021
Christian Puchert <puchert@or.rwth-aachen.de>
2013-04-04 13:34:36 +0200

    script that checks which SCIP plugins have not yet been included in GCG

---
+++ scripts/check_plugins.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#
+# Check which SCIP plugins are included in GCG
+# and print those which are not yet included.
+#
+# Invocation: scripts/check_plugins.sh INCLUDEFILE
+#
+# where INCLUDEFILE is a .c source code file
+# defining which plugins are included in GCG
+# (e.g. gcgplugins.c or masterplugins.c)
+#
+
+INCLUDEFILE=$1
+
+SCIPINCLUDE=lib/scip-git/src/scip/scipdefplugins.c
+
+# List of plugins which should be ignored
+EXCLUDE="
+SCIPincludeConshdlrNonlinear
+SCIPincludeConshdlrQuadratic
+"
+
+
+PLUGINS=`grep -e '^ *SCIP_CALL( *SCIPinclude[A-Za-z0-9]*(scip) *)' $SCIPINCLUDE |
+    sed 's/.*\(SCIPinclude[A-Za-z0-9]*\).*/\1/'`
+
+for PLUGIN in $PLUGINS
+do
+    if [ `grep -c $PLUGIN $INCLUDEFILE` -eq 0 ] &&
+        [ `echo $EXCLUDE | grep -c $PLUGIN` -eq 0 ]
+    then
+        echo $PLUGIN
+    fi
+done
