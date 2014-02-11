a8a8ed0db6e98bd20a8f012eae52ef0a7403cbdc
Gerald Gamrath <gamrath@zib.de>
2014-02-11 16:57:20 +0100

    increased hard time limit in check script

--- check/check.sh
+++ check/check.sh
@@ -133,7 +133,7 @@ date >>$OUTFILE
 date >>$ERRFILE
 
 # we add 50% to the hard time limit and additional 10 seconds in case of small time limits
-HARDTIMELIMIT=`expr \`expr $TIMELIMIT + 10\` + \`expr $TIMELIMIT / 50\``
+HARDTIMELIMIT=`expr \`expr $TIMELIMIT + 10\` + \`expr $TIMELIMIT / 2\``
 
 # we add 10% to the hard memory limit and additional 1000mb to the hard memory limit
 HARDMEMLIMIT=`expr \`expr $MEMLIMIT + 1000\` + \`expr $MEMLIMIT / 10\``
