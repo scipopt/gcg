f959e48e9282c9001a740f7a032fffe8340400a2
Martin Bergner <bergner@or.rwth-aachen.de>
2014-04-07 08:39:25 +0200

    increase version number to 2.0.0.2 because of output and interface changes

--- src/main.c
+++ src/main.c
@@ -34,7 +34,7 @@
 
 /*--+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
 #define GCG_VERSION 200
-#define GCG_SUBVERSION 1
+#define GCG_SUBVERSION 2
 
 #include <string.h>
 
