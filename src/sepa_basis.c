5f88fbb74bbfc54ecb199d2261ebca2725dcb2f6
Jonas Witt <jonas.witt@rwth-aachen.de>
2014-05-25 11:24:42 +0200

    Remove assertion. If origcut is efficacious, mastercut need not be.

--- src/sepa_basis.c
+++ src/sepa_basis.c
@@ -1728,8 +1728,9 @@ SCIP_DECL_SEPAEXECLP(sepaExeclpBasis)
          SCIP_CALL( SCIPcaptureRow(scip, sepadata->mastercuts[sepadata->nmastercuts]) );
          sepadata->nmastercuts++;
          SCIP_CALL( GCGsepaAddMastercuts(scip, origcut, mastercut) );
+
          /* todo: master cut does not have to be efficacious! */
-         assert( SCIPisCutEfficacious(scip, NULL, mastercut) );
+         //assert( SCIPisCutEfficacious(scip, NULL, mastercut) );
 
    #ifdef SCIP_DEBUG
          SCIPdebugMessage("Cut %d:\n", i);
