560b21216dae71f0213482e7f24548a7a4e3c62c
Christian Puchert <puchert@or.rwth-aachen.de>
2014-11-17 09:32:23 +0100

    uncommented parameter settings in relax_gcg.

--- src/relax_gcg.c
+++ src/relax_gcg.c
@@ -943,9 +943,9 @@ SCIP_RETCODE setPricingProblemParameters(
 
    /* disable dual fixing presolver for the moment, because we want to avoid variables fixed to infinity */
    SCIP_CALL( SCIPsetIntParam(scip, "propagating/dualfix/freq", -1) );
-   /*SCIP_CALL( SCIPsetIntParam(scip, "propagating/dualfix/maxprerounds", 0) );*/
+   SCIP_CALL( SCIPsetIntParam(scip, "propagating/dualfix/maxprerounds", 0) );
    SCIP_CALL( SCIPfixParam(scip, "propagating/dualfix/freq") );
-   /*SCIP_CALL( SCIPfixParam(scip, "propagating/dualfix/maxprerounds") );*/
+   SCIP_CALL( SCIPfixParam(scip, "propagating/dualfix/maxprerounds") );
 
    /* disable solution storage ! */
    SCIP_CALL( SCIPsetIntParam(scip, "limits/maxorigsol", 0) );
