964380d1549bbf02773e8945a647c3ac0e27a453
Michael Bastubbe <bastubbe@or.rwth-aachen.de>
2016-02-22 18:53:53 +0100

    initial commit of new classes atm just copies

---
+++ src/class_seeed.h
@@ -0,0 +1,137 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the program                         */
+/*          GCG --- Generic Column Generation                                */
+/*                  a Dantzig-Wolfe decomposition based extension            */
+/*                  of the branch-cut-and-price framework                    */
+/*         SCIP --- Solving Constraint Integer Programs                      */
+/*                                                                           */
+/* Copyright (C) 2010-2015 Operations Research, RWTH Aachen University       */
+/*                         Zuse Institute Berlin (ZIB)                       */
+/*                                                                           */
+/* This program is free software; you can redistribute it and/or             */
+/* modify it under the terms of the GNU Lesser General Public License        */
+/* as published by the Free Software Foundation; either version 3            */
+/* of the License, or (at your option) any later version.                    */
+/*                                                                           */
+/* This program is distributed in the hope that it will be useful,           */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/* GNU Lesser General Public License for more details.                       */
+/*                                                                           */
+/* You should have received a copy of the GNU Lesser General Public License  */
+/* along with this program; if not, write to the Free Software               */
+/* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.*/
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**@file   class_Seeed.h
+ * @brief  class with functions for column pool
+ * @author Jonas Witt
+ */
+
+/*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
+
+#ifndef GCG_CLASS_SEEED_H__
+#define GCG_CLASS_SEEED_H__
+
+#include "objscip/objscip.h"
+#include <vector>
+
+
+
+namespace gcg {
+
+
+
+class Seeed
+{
+
+private:
+   int					id;							/**< id of the seeed */
+   int 					nBlocks;					/**< number of blocks the decomposition currently has */
+   std::vector<int>		masterconss;				/**< vector containing indices of master constraints */
+   std::vector<int>		mastervars;					/**< vector containing indices of master variables */
+   std::vector<std::vector<int>> conssForBlocks; 	/**< conssForBlocks[k] contains a vector of indices of all constraints assigned to block k */
+   std::vector<std::vector<int>> varsForBlocks; 	/**< varsForBlocks[k] contains a vector of indices of all variables assigned to block k */
+
+
+
+public:
+
+   /** constructor */
+   Seeed(
+      SCIP*             scip,               /**< SCIP data structure */
+      int               agelimit,           /**< maximum age a column can reach before it is deleted from the pool */
+      int               maxncolssoft,       /**< soft maximal number of columns stored in the pool at the same time */
+      int               maxncolshard        /**< hard maximal number of columns stored in the pool at the same time */
+      );
+
+   ~Seeed();
+
+   /** add gcg column to column pool */
+   SCIP_RETCODE addCol(
+      GCG_COL*          gcgcol,             /**< gcg column to add */
+      SCIP_Bool*        success             /**< bool returns if colum was succesfully added (number of columns is not bigger than maxncols) */
+   );
+
+   /** return if column already exists in column pool */
+   SCIP_Bool existsCol(
+      GCG_COL*          gcgcol
+      );
+
+   /**< get best column in column pool and remove it from column pool */
+   SCIP_RETCODE getBestCol(
+      GCG_COL**         gcgcol              /**< pointer to store gcg column */
+      );
+
+   /**< get best column's reduced cost */
+   SCIP_Real getBestColRedcost();
+
+   /**< get best column's probnr */
+   int getBestColProbNr();
+
+   /**< get age of column at specific postition */
+   int getColAge(
+      int               pos                 /**< position of column */
+   );
+
+   /**< get reduced cost of column at specific postition */
+   SCIP_Real getColRedcost(
+      int               pos                 /**< position of column */
+      );
+
+   /**< get number of columns in column pool */
+   int getNCols();
+
+   /**< delete all columns that are older than agelimit */
+   SCIP_RETCODE deleteOldColumns();
+
+   /**< delete the oldest columns such that number of columns in Seeed is
+    *   lower than or equal to maxncolssoft */
+   SCIP_RETCODE deleteOldestColumns();
+
+   /**< delete all columns in Seeed */
+   SCIP_RETCODE deleteAllColumns();
+
+   /**< resort columns (after reduce cost have changed) */
+   SCIP_RETCODE resortColumns();
+
+   /**< get columns in column pool */
+   GCG_COL** getCols();
+
+   SCIP_RETCODE setSoftlimit(
+      int               newsoftlimit
+      );
+
+   SCIP_RETCODE updateNode(
+      );
+
+private:
+
+
+
+};
+
+} /* namespace gcg */
+#endif /* GCG_CLASS_Seeed_H__ */
