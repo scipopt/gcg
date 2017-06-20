6342f880ba499d97787834d175b537836628d0ab
Michael Bastubbe <bastubbe@or.rwth-aachen.de>
2017-06-20 15:41:42 +0200

    repaired compiler error

--- src/class_seeedpool.cpp
+++ src/class_seeedpool.cpp
@@ -2906,7 +2906,7 @@ SCIP_RETCODE Seeedpool::createSeeedFromDecomp(
       SeeedPtr*   newseeed                                   /** the new seeed created from the decomp */
   )
 {
-   newseeed = new Seeed(scip, this->getNewIdForSeeed(), this->getNDetectors(), this->getNConss(), this->getNVars() );
+   *newseeed = new Seeed(scip, this->getNewIdForSeeed(), this->getNDetectors(), this->getNConss(), this->getNVars() );
 
    return SCIP_OKAY;
 
