40641bc6613be3406e0d2c3d45dbb880af1dffd1
Michael Bastubbe <bastubbe@or.rwth-aachen.de>
2017-11-23 13:12:58 +0100

    added new mode for amke test: checkexistence to check whether given decomp is already found

--- check/check.sh
+++ check/check.sh
@@ -266,6 +266,29 @@ do
 		echo presolve                      >> $TMPFILE
 		echo detect                        >> $TMPFILE
                 echo display detectionst           >> $TMPFILE
+	    elif test $MODE = "checkexistence"
+	    then
+		echo change instancename $PROB     >> $TMPFILE
+		echo set presolving maxrounds 0    >> $TMPFILE
+		echo presolve                      >> $TMPFILE
+		echo detect                        >> $TMPFILE
+		if test -f $DECFILE
+                    then
+                        BLKFILE=$DECFILE
+                    fi
+                    if test -f $BLKFILE
+                    then
+                        EXT=${BLKFILE##*.}
+                        if test "$EXT" = "gz"
+                        then
+                            presol=`zgrep -A1 PRESOLVE $BLKFILE`
+                        else
+                            presol=`grep -A1 PRESOLVE $BLKFILE`
+                        fi
+                        echo $presol
+                        # If the decomposition contains presolving information ...
+                        echo read $BLKFILE         >> $TMPFILE
+                    fi
             elif test $MODE = "bip"
             then
                 echo presolve                      >> $TMPFILE
