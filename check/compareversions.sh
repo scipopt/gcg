9d0ce994a6216ea024d758e71a131a5d35f183e9
Hanna Franzen <hanna.franzen@rwth-aachen.de>
2018-02-15 14:57:48 +0100

    draft: changed input to simple version

--- check/compareversions.sh
+++ check/compareversions.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-
 #####
 # README: 
-# The script can be called with parameters of the form 
-# "testset" "testparams" "version1" "comparams1" "params1" "version2" "comparams2" "params2" "version3" ...
-# If no such parameters are present the script interactively asks to enter these parameters.
+# The script is called with parameters of the form 
+# "testset testparam1;testparam2;... githash/gitbranch1 param1;param2;... githash/gitbranch2 param1;param2;..."
 # Parameters:
-#	- testset:	Testset to run the different versions on
-#	- testparams:	Parameters for the test script
-#	- versionN:	git branch or hash #N
-#	- comparamsN:	compile parameters for GCG version #N
-#	- paramsN:	parameters with which to run GCG version #N
+#	- testset:		Testset to run the different versions on
+#	- testparams:		Parameters for the test script
+#	- githash/gitbranchN:	GCG versions to be run (a githash or a branch)
+#	- params:		parameters with which to compile & run GCG version #N
 #####
 
 
@@ -22,122 +19,43 @@ USERINPUT=""		# temp var for user inputs
 VERSIONCOUNTER=0	# stores amount of versions to compare
 
 
-# 1) If no params were given ask for testset, versions and parameters, and store them.
-#    Else store testset and fill other parameters into arrays without interaction.
+# 1) Get parameters
 echo ""
 echo "This script will run different versions of GCG using the test script for comparison."
 
 if [ -z $1 ]
 then
-	# Case that no parameters where given when script was started
-	echo "You have not specified what you want to compare yet."
-	echo "If you would like to avoid this dialog in the future please call this script with"
-	echo "\"testset\" \"testparams\" \"version1\" \"comparams1\" \"params1\" \"version2\" \"comparams2\" \"params2\" ... "
-	echo "(where\"testparams\" are parameters for the test script; \"version\" is a git branch or git hash;"
-	echo "GCG should be compiled with \"comparams\" and called with \"params\")."
-	echo ""
-
-	# Let user specify the testset
-	echo "What testset would you like to run?"
-	read USERINPUT
-	while [ -z $USERINPUT ]
-	do
-		echo "You forgot to enter the testset. Try again:"
-		read USERINPUT
-	done
-	TESTSET="$USERINPUT"
-
-	# Let user specify test script parameters (which might be empty)
-	echo "What parameters would you like the test to run with? (Press Enter for no parameters)"
-	read USERINPUT
-	TESTPARAMS="$USERINPUT"
-
-	# Set VERSIONCOUNTER manually
-	echo "How many different versions of GCG would you like to compare?"
-	read USERINPUT
-	# Is input a number? evaluate input arithmetically and compare to itself for quick check
-	if [ -z "$USERINPUT" ] || [ "$USERINPUT" -eq 0 ] || [[ $((USERINPUT)) != "$USERINPUT" ]]
-	then
-		echo "None? Ok, goodbye then."
-		exit 0
-	fi
-	VERSIONCOUNTER="$USERINPUT"
-
-	# Let user enter git version and params (all params might be empty)
-	# Note: for better user interaction the indices of VERSION, COMPARAMS and PARAMS arrays start at 1
-	i=1
-	while [ "$i" -le "$VERSIONCOUNTER" ]
-	do
-		echo "Please enter the git branch name or git hash of GCG version" $i ": "
-		read USERINPUT
-		while [ -z $USERINPUT ]
-		do
-			echo "You forgot to enter the git version. Try again:"
-			read USERINPUT
-		done
-		VERSION[${i}]="$USERINPUT"
-		echo "Please enter the parameters to use for compilation of this GCG version (press Enter for no parameters)."
-		read USERINPUT
-		COMPARAMS[${i}]="$USERINPUT"
-		echo "Please enter the parameters to run this GCG version with (press Enter for no parameters)."
-		read USERINPUT
-		PARAMS[${i}]="$USERINPUT"
-		# Increment loop iterator
-		i=$(($i + 1))
-	done
-else
-	#TODO this case does not work properly (versions & paramss)
-	# Case that parameters where given when script was started:
-	# Go through all input arguments and store them
-	if (($# == 1))
-	then
-		echo "You entered only one argument. Try putting \"\" around each argument."
-		echo "Remember, the proper format for input is:"
-		echo "\"testset\" \"testparams\" \"version1\" \"comparams1\" \"params1\" \"version2\" \"comparams2\" \"params2\" ... "
-		exit 0
-	fi
-	i=0
-	TESTSET=$1
-	shift
-	i=$((i + 1))
-	TESTPARAMS=$1
-	shift
-	i=$((i + 1))
-	VERSIONCOUNTER=0
-	inputtype=1 # 1 is version, 2 is comparam, 3 is param
-	# Go through arguments starting from the third one (first was testset, second was testparams)
-	while ((i > 0))
-	do
-		# check whether the current index belongs to version, comparams or params
-		if ((inputtype = 1))
-		then
-			VERSIONCOUNTER=$((VERSIONCOUNTER + 1))
-			VERSION=("${VERSION[*]}" "$1")
-			inputtype=2
-		elif ((inputtype = 2)); then
-			COMPARAMS=("${COMPARAMS[*]}" "$1")
-			inputtype=3
-		else
-			PARAMS=("${PARAMS[*]}" "$1")
-			inputtype=1
-		fi
-		shift
-		i=$((i - 1))
-	done
-	# Quick sanity check for input
-	if [ "$VERSIONCOUNTER" -eq 0 ]
-	then
-		echo "There are no versions to compare. The correct argument format of this script is:"
-		echo "\"testset\" \"version1\" \"comparams1\" \"params1\" \"version2\" \"comparams2\" \"params2\" ... "
-		echo "Alternatively you can restart this script without arguments to receive interactive help with getting the input right."
-		echo "Goodbye until the next try!"
-		exit 0
-	fi
+	echo "No input to compare. Please call this script as"
+	echo "./compareversions testset testparam1,testparam2,... githash/gitbranch1 param1,param2,... githash/gitbranch2 param1,param2,..."
+	echo "Place \",,\" for empty parameters."
+	exit 0
 fi
-#echo VERSIONCOUNTER= "$VERSIONCOUNTER", VERSION= "${VERSION[*]}", COMPARAMS= "${COMPARAMS[*]}", PARAMS= "${PARAMS[*]}". #TODO debug output
 
+# read & store params
+ninputs=$#
+TESTSET=$1
+ninputs=$((ninputs - 1))
+shift
+TESTPARAMS=$1
+ninputs=$((ninputs - 1))
+shift
+VERSION=""
+PARAMS=""
+nversions=0
+echo $ninputs
+while ((ninputs > 0))
+do
+	nversions=$((nversions + 1))
+	VERSION=("${VERSION[*]}" "$1")
+	ninputs=$((ninputs - 1))
+	shift
+	PARAMS=("${PARAMS[*]}" "$1")
+	ninputs=$((ninputs - 1))
+	shift
+done
+echo VERSIONCOUNTER= "$nversions", VERSION= "${VERSION[*]}", PARAMS= "${PARAMS[*]}". 
 
-# 2) check out the version(s), compile, run with corresponding parameter(s)
+# 2) TODO check out the version(s), compile, run with corresponding parameter(s)
 #		if out files would get overwritten then add version/params coding to their names
 
 # Store current branch to return in the end
@@ -154,15 +72,22 @@ do
 	git submodule init
 	git submodule sync
 	git submodule update
+	make soplex
+	make scip
 	make "${COMPARAMS[${i}]}" deps
 	make "${COMPARAMS[${i}]}"
+
+	# run testset
+	make test TEST="$TESTSET" SETTINGS
+
+	# TODO change name of output files
 
 	i=$((i + 1))
 done
 git checkout "${CURRENTBRANCH}"
 
 
-# 3) if wished do sth with the output, e.g. make summary of differences in summary etc.
+# 3) do sth with the output: 
 
 # termination
 exit 0
