657d1bd4e36a32ea02a5c44b713cf788c7ccc705
Hanna Franzen <hanna.franzen@rwth-aachen.de>
2018-03-22 11:51:36 +0100

    started on parser for res files

---
+++ check/parseres.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# This script reads *.res files created by GCG's make test and parses it into a pandas dataframe.
+# Based on the parseout.py script
+import sys
+import os
+import re
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# check command line arguments
+if len(sys.argv) < 2:
+	sys.exit("Usage: ./parseout.py RESFILE")
+
+# array for all processed lines
+linearray = []
+
+# checkout outfile
+resfile = sys.argv[1]
+fh = open(resfile, 'r')
+
+# write solving information of testset into 
+for line in fh:
+	# get all data lines of first table
+	if '----------' not in line and line not in ['\n', '\r\n'] and not line.startswith("Name   ") and not line.startswith(" ") and not line.startswith("@"):
+		line = " ".join(line.split())
+		linearray.append(line.split(" "))
+
+
