f20283f7e2da19456168181b587bfdf67d92e48f
Matthias Walter <matthias@matthiaswalter.org>
2018-05-31 10:33:41 -0400

    Marked Martins script as (C) OR, too

--- scripts/fix_gcg_code.sh
+++ scripts/fix_gcg_code.sh
@@ -1,5 +1,29 @@
-# this file is licensed under the GNU Lesser General Public License and part of the program GCG
-# Copyright 2012 Martin Bergner
+#* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *#
+#*                                                                           *#
+#*                  This file is part of the program                         *#
+#*          GCG --- Generic Column Generation                                *#
+#*                  a Dantzig-Wolfe decomposition based extension            *#
+#*                  of the branch-cut-and-price framework                    *#
+#*         SCIP --- Solving Constraint Integer Programs                      *#
+#*                                                                           *#
+#* Copyright (C) 2010-2018 Operations Research, RWTH Aachen University       *#
+#*                         Zuse Institute Berlin (ZIB)                       *#
+#*                                                                           *#
+#* This program is free software; you can redistribute it and#or             *#
+#* modify it under the terms of the GNU Lesser General Public License        *#
+#* as published by the Free Software Foundation; either version 3            *#
+#* of the License, or (at your option) any later version.                    *#
+#*                                                                           *#
+#* This program is distributed in the hope that it will be useful,           *#
+#* but WITHOUT ANY WARRANTY; without even the implied warranty of            *#
+#* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *#
+#* GNU Lesser General Public License for more details.                       *#
+#*                                                                           *#
+#* You should have received a copy of the GNU Lesser General Public License  *#
+#* along with this program; if not, write to the Free Software               *#
+#* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.*#
+#*                                                                           *#
+#* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *#
 
 # lowercase first character in doxygen comment
 sed -i 's,\/\*\*\(\ *\)\([A-Z]\)\([a-z]\),\/\*\*\1\L\2\3,' src/*.{c,h}
