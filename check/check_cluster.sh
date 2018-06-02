1717cacf5aca79ac1548cf125dfde1794d7471b3
Benedikt Meier <meier@bene-laptop.bm32.org>
2018-06-02 12:49:16 +0200

    Verbesserung

--- check/check_cluster.sh
+++ check/check_cluster.sh
@@ -240,11 +240,12 @@ do
         TLIMIT=`echo $HARDTIMELIMIT | awk '{ n = split($0,a,":"); print 60*a[1]+a[2];}'`
 		ULIMITMEMLIMIT=`expr $HARDMEMLIMIT \* 1024000`
 
-        AGUMENTS="$CLIENTTMPDIR $CONTINUE $BINNAME $TLIMIT $EVALFILE $JOBFILE $HARDMEMLIMIT $ULIMITMEMLIMIT $SOLVERPATH"
+        AGUMENTS=`echo"$CLIENTTMPDIR $CONTINUE $BINNAME $TLIMIT $EVALFILE $JOBFILE $HARDMEMLIMIT $ULIMITMEMLIMIT $SOLVERPATH"`
         sed -i "s,\$SOLVERPATH,$SOLVERPATH," runcluster_tmp.sub
         sed -i "s,\$GCGPATH,$GCGPATH," runcluster_tmp.sub
         sed -i "s,\$AGUMENTS,$AGUMENTS," runcluster_tmp.sub
         sed -i "s,\$COUNT,$COUNT," runcluster_tmp.sub
+        cat runcluster_tmp.sub
         condor_submit runcluster_tmp.sub
         rm runcluster_tmp.sub
     fi
