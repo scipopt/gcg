0119585225afc1b346d1235e7a9b5e55317092f7
Stephen J. Maher <s.maher3@lancaster.ac.uk>
2018-06-12 10:45:17 +0100

    fixes typo for constype detection

--- src/class_seeedpool.cpp
+++ src/class_seeedpool.cpp
@@ -4174,7 +4174,7 @@ ConsClassifier* Seeedpool::createConsClassifierForMiplibConstypes()
 
 
 
-   classifier = new ConsClassifier( scip, "constypes according to miplip", (int) SCIP_CONSTYPE_GENERAL + 1, getNConss() );
+   classifier = new ConsClassifier( scip, "constypes according to miplib", (int) SCIP_CONSTYPE_GENERAL + 1, getNConss() );
 
 
    /** set class names and descriptions of every class */
