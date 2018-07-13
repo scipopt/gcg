29c81e0b683db5cedd48ebec3aa85062b4adea4b
Hanna Franzen <hanna.franzen@rwth-aachen.de>
2018-07-13 13:23:36 +0200

    percentages and axis alignment for runtimecomparison plot

--- check/plotcomparedres.py
+++ check/plotcomparedres.py
@@ -45,7 +45,7 @@ for resfile in os.listdir(resdir):
 ordereddata = collections.OrderedDict(sorted(datasets.items()))
 orderedsum = collections.OrderedDict(sorted(sumsets.items()))
 
-# Sanity check: check whether the number of tested instances instances differs
+# Sanity check: check whether the number of tested instances differs
 ninstances = -1
 printwarning = False
 for res in filenames:
@@ -176,9 +176,11 @@ plt.savefig(outdir + '/runtimes.pdf')				# name of image
 items = list(runtime.items())
 if len(items) < 2:
 	print 'Enter more than one GCG version to generate a runtime comparison plot.'
-else:
+else:	
 	highestdiff = 0
 	lowestdiff = 0
+	highestcum = 0
+	lowestcum = 0
 	runtimecomp = collections.OrderedDict()
 	cumulative = collections.OrderedDict()
 	for i in range(len(items)):
@@ -187,6 +189,8 @@ else:
 			# from the second item on calculate the version speed differences
 			name = items[i-1][0] + '\n->\n' + items[i][0]
 			diff = int(round( float(items[i-1][1]) - float(items[i][1]) ))
+			maxtime = max(items[i-1][1], items[i][1])
+			diff = float(diff) / maxtime
 			runtimecomp[name] = diff
 			if diff > highestdiff:
 				highestdiff = diff
@@ -195,22 +199,41 @@ else:
 			# for the first one set initial cumulative value
 			if i == 1:
 				cumulative[name] = diff
+				highestcum = diff
+				lowestcum = diff
 			# for all following add the last value to current diff
 			else:
 				cumitems = list(cumulative.items())
 				cumsum = cumitems[len(cumitems)-1][1]
 				cumulative[name] = diff + cumsum
+				if cumulative[name] > highestcum:
+					highestcum = cumulative[name]
+				elif cumulative[name] < lowestcum:
+					lowestcum = cumulative[name]
+
+	#determine axis min/max (leave space for bar labels
+	axmin = lowestdiff+0.1*lowestdiff
+	if lowestcum < axmin:
+		axmin = lowestcum
+	if axmin > 0:
+		axmin = 0
+
+	axmax = highestdiff+0.1*highestdiff
+	if highestcum > axmax:
+		axmax = highestcum
+	if axmax < 0:
+		axmax = 0
 
 	# first plot version-to-version comparison bars
 	fig, ax1 = plt.subplots()
 	bar1 = ax1.bar(range(len(runtimecomp)), runtimecomp.values(), color='b')
 	plt.xticks(range(len(runtimecomp)), runtimecomp.keys(), rotation=90)
 	plt.tick_params(axis='x', which='major', labelsize=5)
-	ax1.set_ylabel('Speedup in seconds', color='b')
+	ax1.set_ylabel('Speedup in percent', color='b')
 	ax1.tick_params('y', colors='b')
 
 	# make space far bar labels
-	ax1.set_ylim(ymin=(lowestdiff+0.1*lowestdiff), ymax=(highestdiff+0.1*highestdiff))
+	ax1.set_ylim(ymin=axmin, ymax=axmax)
 	longestbar = highestdiff	
 	if abs(lowestdiff) > longestbar:
 		longestbar = abs(lowestdiff)
@@ -221,8 +244,10 @@ else:
 	if len(items) > 2:
 		ax2 = ax1.twinx()
 		ax2.plot(range(len(runtimecomp)), cumulative.values(), 'r-')
-		ax2.set_ylabel('Cumulative Speedup in seconds', color='r')
+		ax2.set_ylabel('Cumulative Speedup in percent', color='r')
 		ax2.tick_params('y', colors='r')
+		ax2.axhline(y=0, color='xkcd:orange')
+		ax2.set_ylim(ymin=axmin, ymax=axmax)
 	
 	fig.tight_layout(rect=[0, 0.03, 1, 0.95])
 	plt.title('Version-to-version runtime comparison')
