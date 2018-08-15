5648ab89b0fe1121acafe9ae2ab0eea0208d9555
Christian Puchert <puchert@or.rwth-aachen.de>
2018-08-15 15:56:29 +0200

    add an <include>.

--- src/event_relaxsol.c
+++ src/event_relaxsol.c
@@ -32,6 +32,7 @@
 
 /*--+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
 
+#include <string.h>
 #include "event_relaxsol.h"
 #include "relax_gcg.h"
 #include "pricer_gcg.h"
