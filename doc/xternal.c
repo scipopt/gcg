e2c91f95087eea729918ff5f837d1a2c1d43af1a
Matthias Walter <matthias@matthiaswalter.org>
2019-01-30 10:04:36 +0100

    Preparing for 3.0.2

--- doc/xternal.c
+++ doc/xternal.c
@@ -53,7 +53,7 @@
  * - \ref RELEASENOTES "Release notes"
  * - \ref LICENSE      "Licensing information"
  *
- * @version  3.0.1
+ * @version  3.0.2
  *
  * <b>Further Documentation</b>
  * - \ref IMPORTANTMETHODS "Methods you should know for writing GCG plug-ins"
