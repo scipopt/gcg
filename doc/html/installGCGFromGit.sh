9ad8405840e5205cf84565ee30aa83828b48d748
Tim Donkiewicz <tim.donkiewicz@rwth-aachen.de>
2019-08-04 21:42:03 +0200

    Git install script now also makes all links and the folder to clone to can be given

--- doc/html/installGCGFromGit.sh
+++ doc/html/installGCGFromGit.sh
@@ -1,8 +1,32 @@
+if [ ! -z $1 ];
+  then
+    echo $1
+    NAME=$1
+  else
+    echo "Usage: ./installGCGFromGit.sh <folder name>"
+    exit
+fi
+
+echo "Installing prerequisites..."
 sudo apt-get install gcc gpp git cmake build-essential libgmp-dev libreadline-dev zlib1g-dev bison flex libncurses-dev
-git clone git@git.or.rwth-aachen.de:gcg/gcg.git $1
-cd $1
+
+echo "Cloning repository..."
+git clone git@git.or.rwth-aachen.de:gcg/gcg.git $NAME
+cd $NAME
+
+echo "Getting submodules..."
 git submodule init
 git submodule sync
 git submodule update
+
+echo "Setting links..."
+sudo ln -sfn ../lib/scip-git lib/scip
+# SoPlex links inside SCIP lib folder
+sudo ln -sfn ../../../soplex-git/src/ lib/scip/lib/include/spxinc
+sudo ln -sfn ../../../soplex-git/lib/libsoplex.linux.x86_64.gnu.opt.a lib/scip/lib/static/libsoplex.linux.x86_64.gnu.opt.a
+
+echo "Compiling dependencies..."
 sudo make deps
+
+echo "Compiling GCG..."
 sudo make
