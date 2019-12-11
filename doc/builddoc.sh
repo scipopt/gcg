e2a86fb26a776acbb98359bbb43a4b0ae29e1eb6
Tim Donkiewicz <tim.donkiewicz@rwth-aachen.de>
2019-12-11 20:16:21 +0100

    js/css/bootstrap/fonts now downloaded during doc generation - docu version specific js/css/... ! - path changes

--- doc/builddoc.sh
+++ doc/builddoc.sh
@@ -17,7 +17,6 @@ if [[ -z ${BINDIR} ]]; then export BINDIR="$PWD/../bin"; fi
 cd $(dirname $0)
 
 mkdir -p html
-cp -r resources/misc/scripts html
 
 if [ "$1" == "--mathjax" ]
 then
@@ -27,7 +26,7 @@ then
       echo "Updating git repository for MathJax."
       cd html/MathJax
       git checkout 2.7.7
-      rm *.md
+      rm -f *.md
       cd ../..
    else
       echo "Cloning git repository for MathJax."
@@ -50,25 +49,54 @@ fi
 CURRENT_VERSION=`grep '@version' resources/main.md | awk '{ printf("%s", $2); }'`
 
 echo "Building documentation in html/doc-${CURRENT_VERSION}."
-echo "  Generating FAQ..."
-echo "    Please ensure that you have php installed."
+echo " Getting required CSS, .js and picture files..."
+mkdir -p html/bootstrap/css
+mkdir -p html/bootstrap/js
+mkdir -p html/css
+mkdir -p html/js
+mkdir -p html/img
+
+# Getting Bootstrap stuff
+wget https://scip.zib.de/bootstrap/css/bootstrap.min.css --output-document html/bootstrap/css/bootstrap.min.css --quiet
+wget https://scip.zib.de/bootstrap/css/custom.css --output-document html/bootstrap/css/custom.css --quiet
+sed -i 's/https:\/\/scip.zib.de\/images/..\/..\/img/g' html/bootstrap/css/custom.css
+# Getting fonts and css
+wget https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css --output-document html/css/font-awesome.min.css --quiet
+wget https://fonts.googleapis.com/css?family=Open+Sans --output-document html/css/font-googleapis.css --quiet
+wget https://fonts.gstatic.com/s/opensans/v17/mem8YaGs126MiZpBA-UFW50bbck.woff2 --output-document html/css/font-googleapis.woff2 --quiet
+# Getting js
+wget https://scip.zib.de/bootstrap/js/custom.js --output-document html/bootstrap/js/custom.js --quiet
+wget https://scip.zib.de/bootstrap/js/bootstrap.min.js --output-document html/bootstrap/js/bootstrap.min.js --quiet
+wget https://code.jquery.com/jquery.min.js --output-document html/js/jquery.min.js --quiet
+
+echo "Generating FAQ..."
+echo " Requirement: Correctly installed php."
 cd resources/misc/faq
 python parser.py --linkext $HTML_FILE_EXTENSION  && php localfaq.php > faq.inc
 cd ../../../
 echo "<li><a href='../doc-${CURRENT_VERSION}/index.html'>GCG ${CURRENT_VERSION}</a></li>" > docversions.html
 
-echo "  Moving visualization images..."
+echo " Moving resources to doc folder..."
+cp -r resources/misc/scripts html
 mkdir -p html/doc/img/visu
-cp -r ./resources/devs/howtouse/visualizations/img/* ./html/doc/img/visu
+mkdir -p html/doc/bootstrap
+mkdir -p html/doc/js
+mkdir -p html/doc/css
+cp -r resources/devs/howtouse/visualizations/img/* html/doc/img/visu
+cp -r html/bootstrap html/doc
+cp -r html/js html/doc
+cp -r html/css html/doc
+cp -r html/img/newscippy.png html/doc/img/
+cp -r html/img/scribble_light_@2X.png html/doc/img/
 
 echo "Generating parameters file."
-echo "  Please ensure that GCG was installed correctly."
+echo " Requirement: Correctly installed GCG"
 cd ..
 "$BINDIR"/gcg -c "set default set save doc/resources/misc/parameters.set quit" > /dev/null 2>&1
 cd doc
 
 echo "Generating Doxygen docu..."
-echo "  Please ensure that graphviz is installed on your system."
+echo " Requirement: Correctly installed graphviz"
 # Create index.html and gcgheader.html.
 SCIPOPTSUITEHEADER=`sed 's/\//\\\\\//g' scipoptsuiteheader.html.in | tr -d '\n'`
 DOCVERSIONS=`sed 's/\//\\\\\//g' docversions.html | tr -d '\n'`
@@ -79,8 +107,11 @@ sed -e "s/<SCIPOPTSUITEHEADER\/>/${SCIPOPTSUITEHEADER}/g" -e "s/<DOCVERSIONS\/>/
 # Build the gcg documentation.
 DOXYGEN_USE_MATHJAX=${DOXYGEN_USE_MATHJAX} doxygen gcg.dxy
 
+#pwd
+#ls html/doc
+#sed -i 's/https:\/\/scip.zib.de\/images//g' html/doc/bootstrap/css/custom.css
 
-echo "Cleaning up..."
+printf "\nCleaning up...\n"
 rm -rf html/doc-${CURRENT_VERSION} docversions.html gcgheader.html
 mv html/doc html/doc-${CURRENT_VERSION}
 
