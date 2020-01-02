f61e6085662b70af88ec478edbb3cf507971c2a0
Tim Donkiewicz <tim.donkiewicz@rwth-aachen.de>
2020-01-03 00:53:26 +0100

    additional sed standardization

--- doc/resources/devs/howtoadd/createindexes.sh
+++ doc/resources/devs/howtoadd/createindexes.sh
@@ -3,8 +3,8 @@
 
 cd $(dirname $0)
 # Remove ending and subpages of .dox file
-sed -i '/\*\//d' howtoadd.dox
-sed -i '/\\subpage /d' howtoadd.dox
+sed -i'' '/\*\//d' howtoadd.dox
+sed -i'' '/\\subpage /d' howtoadd.dox
 
 # Get index list and append to .dox
 ls | egrep '\.md$|\.dox$' | sed 's/.dox//' | sed 's/.md//' | sed 's/^/- \\subpage /' | sed '/howto/d' >> howtoadd.dox
