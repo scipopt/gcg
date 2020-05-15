4f3f7001660cb231b8e95240a11391094a95bb6e
Tim Donkiewicz <tim.donkiewicz@rwth-aachen.de>
2020-05-15 12:23:03 +0200

    Merge branch 'master' into 433-overall-documentation

---
+++ doc/resources/devs/detection/detectors/det-dbscan.md
@@ -0,0 +1,26 @@
+# Clustering (DBSCAN) {#det-dbscan}
+> **This page is still in development and may be incomplete. Please excuse any inconveniences.**
+
+This detector performs DBSCAN clustering.
+
+| ID |          Full Name          | Propagate | Finish | Postprocess |
+|----|-----------------------------|:---------:|:------:|:-----------:|
+| ?  | dbscan                      | ✓ |   |   |
+
+
+### Details
+#### Distance Measure
+For the distance measure, different similarities can be used. Those are:
+ * Johnson Similarity
+ * Intersection Similarity
+ * Jaccard Similarity
+ * Cosine Similarity
+ * Simpson Similarity
+
+### Parameters
+```
+
+```
+
+### Links
+ * Documentation: dec_dbscan.cpp
--- doc/xternal.c
+++ doc/xternal.c
@@ -1,3 +1,34 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the program                         */
+/*          GCG --- Generic Column Generation                                */
+/*                  a Dantzig-Wolfe decomposition based extension            */
+/*                  of the branch-cut-and-price framework                    */
+/*         SCIP --- Solving Constraint Integer Programs                      */
+/*                                                                           */
+/* Copyright (C) 2010-2020 Operations Research, RWTH Aachen University       */
+/*                         Zuse Institute Berlin (ZIB)                       */
+/*                                                                           */
+/* This program is free software; you can redistribute it and/or             */
+/* modify it under the terms of the GNU Lesser General Public License        */
+/* as published by the Free Software Foundation; either version 3            */
+/* of the License, or (at your option) any later version.                    */
+/*                                                                           */
+/* This program is distributed in the hope that it will be useful,           */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/* GNU Lesser General Public License for more details.                       */
+/*                                                                           */
+/* You should have received a copy of the GNU Lesser General Public License  */
+/* along with this program; if not, write to the Free Software               */
+/* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.*/
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**@file   xternal.c
+ * @brief  documentation page for GCG's C-API (no other pages)
+ */
+
 /**@defgroup PUBLICAPI Public API of \GCG
  * @brief methods and headers of the public C-API of \GCG
  *
