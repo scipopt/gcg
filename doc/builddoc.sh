ab2c118be5d33c986663f17649f208cb11a6765d
Tim Donkiewicz <tim.donkiewicz@rwth-aachen.de>
2020-05-18 10:36:38 +0200

    reintegrate mathjax, UX improvements

--- doc/builddoc.sh
+++ doc/builddoc.sh
@@ -55,20 +55,21 @@ checkMathjax () {( set -e
      DOXYGEN_USE_MATHJAX="YES"
      if [ -d html/MathJax ]
      then
-        echo "Updating git repository for MathJax."
+        printf ": updating repository\n"
         cd html/MathJax
-        git checkout 2.7.7
+        git checkout 2.7.7 > /dev/null 2>&1 
         rm -f *.md
         cd ../..
      else
-        echo "Cloning git repository for MathJax."
+        printf ": cloning repository\n"
         cd html
-        git clone https://github.com/mathjax/MathJax.git --branch=2.7.7 --single-branch --depth 1
+        git clone https://github.com/mathjax/MathJax.git --branch=2.7.7 --single-branch --depth 1 > /dev/null 2>&1 
         rm MathJax/*.md
         cd ..
      fi
   else
-     DOXYGEN_USE_MATHJAX="NO"
+    printf ": compiling without mathjax\n"
+    DOXYGEN_USE_MATHJAX="NO"
   fi
 )}
 
@@ -123,11 +124,12 @@ generateParamsFile () {( set -e
 
 # Remove citelist.html (the Bibliography) manually from the menu (but still reachable via link)
 removeBibliography () {( set -e
-  cd html/doc-${CURRENT_VERSION}
+  cd html/doc
   sed -i.bak "/citelist/d" pages.html && rm pages.html.bak
   sed -i.bak "/citelist/d" navtreedata.js && rm navtreedata.js.bak
   sed -i.bak "s/\:\[5/\:\[4/g" navtreeindex*.js && rm navtreeindex*.js.bak # citelist is the third item in the navigation (after Users Guide and Devs Guide,
   sed -i.bak "s/\:\[6/\:\[5/g" navtreeindex*.js && rm navtreeindex*.js.bak # since Installation counts as homepage and thus 0)
+  cd - > /dev/null 2>&1
 )}
 
 # Create Doxygen documentation for pages and source code
@@ -142,57 +144,68 @@ generateDoxy () {( set -e
   SCIPOPTSUITEHEADER=`sed 's/\//\\\\\//g' scipoptsuiteheader.html.in | tr -d '\n'`
   DOCVERSIONS=`sed 's/\//\\\\\//g' docversions.html | tr -d '\n'`
   YEAR=`date +"%Y"`
-
+  
+  # Replace year by current and version by installed one
   sed -e "s/<SCIPOPTSUITEHEADER\/>/${SCIPOPTSUITEHEADER}/g" -e "s/<DOCVERSIONS\/>/${DOCVERSIONS}/g" -e "s/..\/doc/doc/g" -e "s/<YEAR\/>/${YEAR}/g" -e "s/<CURRGCG\/>/${CURRENT_VERSION}/g" < index.html.in > html/index.html
   sed -e "s/<SCIPOPTSUITEHEADER\/>/${SCIPOPTSUITEHEADER}/g" -e "s/<DOCVERSIONS\/>/${DOCVERSIONS}/g" < gcgheader.html.in > gcgheader.html
 
+  # Set mathjax flag to export it
+  DOXYGEN_USE_MATHJAX="USE_MATHJAX=${DOXYGEN_USE_MATHJAX}"
+  
   # Build the gcg documentation.
-  printf "${R}"
-  DOXYGEN_USE_MATHJAX=${DOXYGEN_USE_MATHJAX} doxygen gcg.dxy
-  printf "${W}"
+  printf "${R}" # make doxygen errors red
+  export ${DOXYGEN_USE_MATHJAX}; doxygen gcg.dxy
+  printf "${W}" 
 )}
 
 main () {
-  # The ||-structure below is a try-catch workaround for bash to alert the user 
+  # The $? conditions below are a try-catch method to alert the user 
   # about the origin of the issue
 
-  n=7
-
+  n=8 # number of steps to be performed
+  i=1 # step counter
+  
   printf "${B}${U}Building GCG HTML Documentation in html/doc-${CURRENT_VERSION}${W}\n"
   mkdir -p html
 
+  # Requirement: Correctly installed git
+  printf "[${i}/${n}] Checking mathjax status"; let "i++"
+    checkMathjax $1
+    
   # Requirement: Internet connection
-  echo "[1/${n}] Downloading additional resources"
-    getAdditionalResources 
+  echo "[${i}/${n}] Downloading additional resources"; let "i++"
+    getAdditionalResources
     if [ $? -ne 0 ]; then printf " ${R}Error:${W} Please check your internet connection.\n"; fi
 
   # Requirement: none
-  echo "[2/${n}] Generating subpage indexing"
+  echo "[${i}/${n}] Generating subpage indexing"; let "i++"
     makeSubpageIndexing "resources/devs/howtoadd/" "How to add"
     makeSubpageIndexing "resources/devs/howtouse/" "How to use"
   
+  # Requirement: Correctly installed php
+  echo "[${i}/${n}] Generating FAQ"; let "i++"
+    generateFAQ
+    if [ $? -ne 0 ]; then printf " ${R}Error:${W} Have you installed PHP and python3 correctly?\n"; fi
+    
   # Requirement: Correctly installed GCG
-  echo "[3/${n}] Generating GCG interactive menu documentation"
-    makeInteractiveMenuDocu
-    if [ $? -ne 0 ]; then printf " ${R}Error:${W} Have you installed GCG correctly?\n"; fi
-  
-  # Requirement: Correctly installed GCG
-  echo "[4/${n}] Generating GCG parameters file"
+  echo "[${i}/${n}] Generating GCG parameters file"; let "i++"
     generateParamsFile
     if [ $? -ne 0 ]; then printf " ${R}Error:${W} Have you installed GCG correctly?\n"; fi
-  
-  # Requirement: Correctly installed php
-  echo "[5/${n}] Generating FAQ"
-    generateFAQ
-    if [ $? -ne 0 ]; then printf " ${R}Error:${W} Have you installed PHP correctly?\n"; fi
+    
+  # Requirement: Correctly installed GCG
+  echo "[${i}/${n}] Generating GCG interactive menu documentation"; let "i++"
+    makeInteractiveMenuDocu
+    if [ $? -ne 0 ]; then printf " ${R}Error:${W} Have you installed GCG and python3 correctly?\n"; fi
   
   # Requirement: Doxygen, graphviz
-  echo "[6/${n}] Generating Doxygen documentation"
+  echo "[${i}/${n}] Generating Doxygen documentation"; let "i++"
     generateDoxy   
     if [ $? -ne 0 ]; then printf " ${R}Error:${W} Have you installed Doxygen and graphviz correctly?\n"; fi
   
   # Requirement: none
-  echo "[7/${n}] Finalizing"
+  echo "[${i}/${n}] Finalizing"
+    # remove bibliography page (used for the use case pages)
+    removeBibliography
     # remove old documentation under this name
     rm -rf html/doc-${CURRENT_VERSION} gcgheader.html
     # move freshly generated docu into the desired (versionized) folder
@@ -201,4 +214,4 @@ main () {
   printf "${B}Done!${W}\n"
 }
 
-main
+main $@
