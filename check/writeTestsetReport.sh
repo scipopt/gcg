08787f457d3abd8189db659970044c81f9d9daaa
Erik Mühmer <muehmer@or.rwth-aachen.de>
2021-10-20 11:09:18 +0200

    update copyrights

--- check/writeTestsetReport.sh
+++ check/writeTestsetReport.sh
@@ -7,7 +7,7 @@
 #*                  of the branch-cut-and-price framework                    *
 #*         SCIP --- Solving Constraint Integer Programs                      *
 #*                                                                           *
-#* Copyright (C) 2010-2020 Operations Research, RWTH Aachen University       *
+#* Copyright (C) 2010-2021 Operations Research, RWTH Aachen University       *
 #*                         Zuse Institute Berlin (ZIB)                       *
 #*                                                                           *
 #* This program is free software; you can redistribute it and/or             *
@@ -473,7 +473,7 @@ cat > ${REPORTFILE} << EndOfMessage
 % *                  of the branch-cut-and-price framework                    *
 % *         SCIP --- Solving Constraint Integer Programs                      *
 % *                                                                           *
-% * Copyright (C) 2010-2020 Operations Research, RWTH Aachen University       *
+% * Copyright (C) 2010-2021 Operations Research, RWTH Aachen University       *
 % *                         Zuse Institute Berlin (ZIB)                       *
 % *                                                                           *
 % * This program is free software; you can redistribute it and/or             *
