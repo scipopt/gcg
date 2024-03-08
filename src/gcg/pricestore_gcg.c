3b7ca2584db7f8fb53a892f5bad1203acea68a2a
Erik Mühmer <muehmer@or.rwth-aachen.de>
2024-03-08 15:32:40 +0100

    move assertion

--- src/gcg/pricestore_gcg.c
+++ src/gcg/pricestore_gcg.c
@@ -292,6 +292,19 @@ SCIP_RETCODE GCGpricestoreAddCol(
    /* If the column is no duplicate of an existing one, add it */
    if( oldpos == -1 )
    {
+#ifndef NDEBUG
+      int i;
+      SCIP_VAR* var;
+      SCIP_Real val;
+      for( i = 0; i < col->nvars; ++i )
+      {
+         var = col->vars[i];
+         val = col->vals[i];
+
+         assert(SCIPisFeasGE(col->pricingprob, val, SCIPvarGetLbGlobal(GCGpricingVarGetOrigvars(var)[0])) &&
+                SCIPisFeasLE(col->pricingprob, val, SCIPvarGetUbGlobal(GCGpricingVarGetOrigvars(var)[0])));
+      }
+#endif
       /* get enough memory to store the col */
       SCIP_CALL( pricestoreEnsureColsMem(pricestore, pricestore->ncols+1) );
       assert(pricestore->ncols < pricestore->colssize);
