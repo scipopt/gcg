cbe1be874cfdcce1346800e12905ddb2d3120282
Erik Mühmer <muehmer@or.rwth-aachen.de>
2024-05-13 19:07:59 +0200

    include json lib only in source files

--- src/gcg/reader_ndec.hpp
+++ src/gcg/reader_ndec.hpp
@@ -36,7 +36,6 @@
 #ifndef GCG_READER_NDEC_HPP__
 #define GCG_READER_NDEC_HPP__
 
-#include <jansson.h>
 #include <vector>
 #include <unordered_map>
 #include <string>
@@ -46,7 +45,6 @@ namespace gcg
 {
 
 struct DecompositionData;
-struct NDecFileHandler;
 
 struct BlockData
 {
@@ -84,127 +82,6 @@ struct NestedDecompositionData
    bool presolved;
    std::string description;
    DecompositionData* rootdecomposition;
-};
-
-class AbstractElementParser
-{
-public:
-   explicit AbstractElementParser(SCIP* scip, NDecFileHandler& filehandler)
-      : scip_(scip), filehandler_(filehandler), error_(false) {}
-
-   virtual ~AbstractElementParser() = default;
-
-   virtual void handleKeyValuePair(const char* name, json_t* value) = 0;
-
-   virtual void handleValue(json_t* value) = 0;
-
-   bool error() const
-   {
-      return error_;
-   }
-
-protected:
-   NDecFileHandler& filehandler_;
-   SCIP* scip_;
-   bool error_;
-};
-
-struct NDecFileHandler
-{
-public:
-   NDecFileHandler(SCIP* scip, const char* filename);
-   NDecFileHandler(SCIP* scip, FILE* wfile);
-   ~NDecFileHandler();
-
-   SCIP_RETCODE initialize();
-
-   bool parseElement(AbstractElementParser& elementparser, json_t* element);
-
-   bool readNDec(NestedDecompositionData& data);
-
-   bool writeNDec(PARTIALDECOMP* decomp);
-
-private:
-   bool serializeBlock(json_t* json, PARTIALDECOMP* decomp, int block);
-   bool serializeBlockStructure(json_t* json, PARTIALDECOMP* decomp, BLOCK_STRUCTURE* blockstructure);
-   bool serializeBlockStructureBlock(json_t* json, PARTIALDECOMP* decomp, BLOCK_STRUCTURE* blockstructure, int block);
-   bool serializeDecomposition(json_t* json, PARTIALDECOMP* decomp);
-   bool setObjectValue(const char* key, json_t* value, json_t* object = NULL, bool decref = true);
-   bool appendArrayValue(json_t* value, json_t* array, bool decref = true);
-
-   static size_t jsonLoadCallback(void* buffer, size_t buflen, void* data);
-   static int jsonDumpCallback(const char* buffer, size_t buflen, void* data);
-
-   SCIP_FILE* rfile_;
-   FILE* wfile_;
-   json_t* json_;
-   json_error_t error_;
-   SCIP* scip_;
-};
-
-class AbstractNestedDecompositionElementParser : public AbstractElementParser
-{
-public:
-   AbstractNestedDecompositionElementParser(SCIP* scip, NDecFileHandler& filehandler, NestedDecompositionData& data)
-      : AbstractElementParser(scip, filehandler), data_(data) {}
-
-   ~AbstractNestedDecompositionElementParser() override = default;
-
-protected:
-   DecompositionData* parseDecomposition(json_t* value);
-
-   NestedDecompositionData& data_;
-};
-
-class BlockElementParser : public AbstractNestedDecompositionElementParser
-{
-public:
-   BlockElementParser(SCIP* scip, NDecFileHandler& filehandler, NestedDecompositionData& data,
-      BlockData& blockdata) : AbstractNestedDecompositionElementParser(scip, filehandler, data), blockdata_(blockdata),
-      parsingconstraints(false) {}
-
-   ~BlockElementParser() override = default;
-
-   void handleKeyValuePair(const char* name, json_t* value) override;
-
-   void handleValue(json_t* value) override;
-
-private:
-   BlockData& blockdata_;
-   bool parsingconstraints;
-};
-
-class DecompositionElementParser : public AbstractNestedDecompositionElementParser
-{
-public:
-   DecompositionElementParser(SCIP* scip, NDecFileHandler& filehandler, NestedDecompositionData& data,
-      DecompositionData& decdata) : AbstractNestedDecompositionElementParser(scip, filehandler, data),
-      decdata_(decdata), parsingmasterconstraints(false), parsingblocks(false), parsingsymmetry(false) {}
-
-   ~DecompositionElementParser() override = default;
-
-   void handleKeyValuePair(const char* name, json_t* value) override;
-
-   void handleValue(json_t* value) override;
-
-private:
-   DecompositionData& decdata_;
-   bool parsingmasterconstraints;
-   bool parsingblocks;
-   bool parsingsymmetry;
-};
-
-class RootElementParser : public AbstractNestedDecompositionElementParser
-{
-public:
-   RootElementParser(SCIP* scip, NDecFileHandler& filehandler, NestedDecompositionData& data)
-      : AbstractNestedDecompositionElementParser(scip, filehandler, data) {}
-
-   ~RootElementParser() override = default;
-
-   void handleKeyValuePair(const char* name, json_t* value) override;
-
-   void handleValue(json_t* value) override;
 };
 
 }
