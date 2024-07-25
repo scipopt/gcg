edf7292d0b8bded14f0c55b49412be69c93a8ccd
Erik Mühmer <muehmer@or.rwth-aachen.de>
2024-07-25 16:41:04 +0200

    update version

--- src/gcg/def.h
+++ src/gcg/def.h
@@ -59,7 +59,7 @@
 #endif
 #endif
 
-#define GCG_VERSION         362 /**< GCG version number (multiplied by 100 to get integer number) */
+#define GCG_VERSION         363 /**< GCG version number (multiplied by 100 to get integer number) */
 #define GCG_SUBVERSION        0 /**< GCG sub version number */
 
 #endif
