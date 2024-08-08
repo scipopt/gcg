732516f3518da23045c964c52a383924be1cd1b3
Til Valentin Mohr <til.mohr@rwth-aachen.de>
2024-08-08 19:32:07 +0200

    Fix disable define Stats

--- src/gcg/branch_compbnd.c
+++ src/gcg/branch_compbnd.c
@@ -33,7 +33,8 @@
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
 
-#define STATS FALSE // will write information about the component bound sequences to a file (slow!)
+// will write information about the component bound sequences to a file (slow!)
+#define STATS
 
 #include <gcg/pricer_gcg.h>
 #include <scip/pub_message.h>
