ce41ddaea57550068d9f6976bef40bbd29733b5e
johannesehls <johannes.ehls@rwth-aachen.de>
2025-03-18 17:06:42 +0100

    fix faulty license headers (old GPL license) in files in src directory

--- src/gcg/gcggithash.c
+++ src/gcg/gcggithash.c
@@ -1,7 +1,7 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 /*                                                                           */
 /*                  This file is part of the program                         */
-/*          GCG --- Generic Colum Generation                                 */
+/*          GCG --- Generic Column Generation                                */
 /*                  a Dantzig-Wolfe decomposition based extension            */
 /*                  of the branch-cut-and-price framework                    */
 /*         SCIP --- Solving Constraint Integer Programs                      */
