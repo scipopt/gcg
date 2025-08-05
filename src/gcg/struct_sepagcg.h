37db09a76cf01f6ee77c1bf0550e0ad9b90babf2
Erik Mühmer <muehmer@or.rwth-aachen.de>
2025-08-05 15:51:55 +0200

    add newlines

--- src/gcg/struct_sepagcg.h
+++ src/gcg/struct_sepagcg.h
@@ -30,6 +30,7 @@
  * @ingroup DATASTRUCTURES
  * @brief   data structures for separators for master
  * @author  Chantal Reinartz Groba
+ * @author  Erik Muehmer
  */
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
